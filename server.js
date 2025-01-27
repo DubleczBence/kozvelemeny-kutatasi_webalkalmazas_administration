f682a77a60687bc92560d640139a758cce7cddd3
DubleczBence <dubleczbence@gmail.com>
2025-01-27 18:58:17 +0100

    20250127_7
    users get végpont

--- server.js
+++ server.js
@@ -44,6 +44,13 @@ app.post('/api/companies', (req, res) => {
 });
 
 
+app.get('/api/users', (req, res) => {
+    db.all('SELECT * FROM users', [], (err, rows) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(rows);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
