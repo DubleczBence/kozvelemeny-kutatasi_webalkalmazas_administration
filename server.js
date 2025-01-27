27d47d31c5202d172b81fc7ce6697a1ad0e3085e
DubleczBence <dubleczbence@gmail.com>
2025-01-27 18:42:02 +0100

    20250127_6
    companies vegpont

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./data');
-const { userValidation } = require('./validation');
+const { userValidation, companyValidation } = require('./validation');
 
 const app = express();
 const port = 3000;
@@ -20,6 +20,26 @@ app.post('/api/users', (req, res) => {
         function(err) {
             if (err) return res.status(500).json({ error: err.message });
             res.json({ id: this.lastID, name, email });
+        });
+});
+
+
+
+app.post('/api/companies', (req, res) => {
+    const { error } = companyValidation(req.body);
+    if (error) return res.status(400).json({ message: error.details[0].message });
+
+    const { cegnev, telefon, ceg_email, jelszo, telepules, megye, 
+            ceges_szamla, hitelkartya, adoszam, cegjegyzek, helyrajziszam } = req.body;
+    
+    db.run(`INSERT INTO companies (cegnev, telefon, ceg_email, jelszo, telepules, megye, 
+            ceges_szamla, hitelkartya, adoszam, cegjegyzek, helyrajziszam) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        [cegnev, telefon, ceg_email, jelszo, telepules, megye, 
+         ceges_szamla, hitelkartya, adoszam, cegjegyzek, helyrajziszam],
+        function(err) {
+            if (err) return res.status(500).json({ error: err.message });
+            res.json({ id: this.lastID, ...req.body });
         });
 });
 
