1e41f51a17050cfa6f7d31eec521d92e2fb37992
DubleczBence <dubleczbence@gmail.com>
2025-01-27 18:07:39 +0100

    20250127_4
    company validálás

--- validation.js
+++ validation.js
@@ -9,6 +9,24 @@ const userValidation = (user) => {
     return schema.validate(user);
 };
 
+const companyValidation = (company) => {
+    const schema = Joi.object({
+        cegnev: Joi.string().min(2).max(255).required(),
+        telefon: Joi.number().required(),
+        ceg_email: Joi.string().email().required(),
+        jelszo: Joi.string().min(6).required(),
+        telepules: Joi.string().required(),
+        megye: Joi.string().required(),
+        ceges_szamla: Joi.number().required(),
+        hitelkartya: Joi.number().required(),
+        adoszam: Joi.number().required(),
+        cegjegyzek: Joi.string().required(),
+        helyrajziszam: Joi.string().required()
+    });
+    return schema.validate(company);
+};
+
 module.exports = {
     userValidation,
+    companyValidation
 };
