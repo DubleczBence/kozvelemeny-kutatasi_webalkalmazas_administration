99fa35482ea0249ac3833a05fe4f3b262ff08d82
DubleczBence <dubleczbence@gmail.com>
2025-01-27 17:26:22 +0100

    20250127_2
    companies tábla

--- data.js
+++ data.js
@@ -10,6 +10,23 @@ db.serialize(() => {
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
     )`);
+
+    
+    db.run(`CREATE TABLE IF NOT EXISTS companies (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cegnev TEXT NOT NULL,
+        telefon INTEGER NOT NULL,
+        ceg_email TEXT UNIQUE NOT NULL,
+        jelszo TEXT NOT NULL,
+        telepules TEXT NOT NULL,
+        megye TEXT NOT NULL,
+        ceges_szamla INTEGER NOT NULL,
+        hitelkartya INTEGER NOT NULL,
+        adoszam INTEGER NOT NULL,
+        cegjegyzek TEXT NOT NULL,
+        helyrajziszam TEXT NOT NULL,
+        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`);
 });
 
 module.exports = db;
